c9706db0f8eddc4f8ccb8e8e6616c1bf240d99ff
Michal Valasek <michal.valasek@gmail.com>
2015-04-22 16:01:04 +0200

    Add Templating to he  the deps in the package.js file.

--- package.js
+++ package.js
@@ -8,14 +8,15 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
+  api.use('templating@1.0.0');
   api.use('aldeed:autoform@5.0.0');
   api.addFiles([
+    'lib/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js',
+    'lib/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css',
+    'lib/bootstrap-colorpicker/dist/img/bootstrap-colorpicker/alpha.png',
+    'lib/bootstrap-colorpicker/dist/img/bootstrap-colorpicker/hue.png',
+    'lib/bootstrap-colorpicker/dist/img/bootstrap-colorpicker/saturation.png',
     'autoform-bootstrap-colorpicker.html',
-    'autoform-bootstrap-colorpicker.js',
-    'lib/bootstrap-colorpicker/js/bootstrap-colorpicker.js',
-    'lib/bootstrap-colorpicker/css/bootstrap-colorpicker.css',
-    'lib/bootstrap-colorpicker/img/bootstrap-colorpicker/alpha.png',
-    'lib/bootstrap-colorpicker/img/bootstrap-colorpicker/hue.png',
-    'lib/bootstrap-colorpicker/img/bootstrap-colorpicker/saturation.png'
+    'autoform-bootstrap-colorpicker.js'
   ], 'client');
 });
